8ea864cbe18b1f9f0c07405614b733b89cca5438
David <davidfilipeamorim@gmail.com>
2020-10-13 20:08:50 +0100

    Create orphanages route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
 
 import './database/connection';
 
@@ -6,7 +8,31 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    } = req.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+    });
+
+    await orphanageRepository.save(orphanage);
+
     return res.json({ message: 'Hello World' });
 });
 
