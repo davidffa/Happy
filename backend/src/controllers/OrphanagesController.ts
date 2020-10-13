fea0c3fb8a80564f122a31bfd1a14199c86df6ca
David <davidfilipeamorim@gmail.com>
2020-10-13 21:32:46 +0100

    Create orphanage list and detail

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,24 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+    async index(req: Request, res: Response) {
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanageRepository.find();
+
+        return res.json(orphanages);
+    },
+
+    async show(req: Request, res: Response) {
+        const { id } = req.params;
+
+        const orphanageRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanageRepository.findOneOrFail(id);
+
+        return res.json(orphanage);
+    },
+
     async create(req: Request, res: Response) {
         const {
             name,
