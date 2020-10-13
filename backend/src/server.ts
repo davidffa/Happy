391055f0c672dfae7a2a87ce579b96fdb08abe9b
David <davidfilipeamorim@gmail.com>
2020-10-13 21:20:12 +0100

    Create routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,11 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = req.body;
-
-    const orphanageRepository = getRepository(Orphanage);
-
-    const orphanage = orphanageRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    });
-
-    await orphanageRepository.save(orphanage);
-
-    return res.json({ message: 'Hello World' });
-});
+app.use(routes);
 
 app.listen(3333);
