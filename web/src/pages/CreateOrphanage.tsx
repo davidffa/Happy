c69d2cac0ba71db8aba341e87f0ff4027e09e7a1
David <davidfilipeamorim@gmail.com>
2020-10-14 15:19:50 +0100

    Add sidebar component

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import L from 'leaflet';
-import { useHistory } from "react-router-dom";
 
-import { FiArrowLeft, FiPlus } from "react-icons/fi";
+import { FiPlus } from "react-icons/fi";
 
 import mapMarkerImg from '../images/map-marker.svg';
+import Sidebar from "../components/Sidebar";
 
 import '../styles/pages/create-orphanage.css';
 
@@ -18,19 +18,9 @@ const happyMapIcon = L.icon({
 })
 
 export default function CreateOrphanage() {
-    const { goBack } = useHistory();
-
     return (
         <div id="page-create-orphanage">
-            <aside>
-                <img src={mapMarkerImg} alt="Happy" />
-
-                <footer>
-                    <button type="button" onClick={goBack}>
-                        <FiArrowLeft size={24} color="#FFF" />
-                    </button>
-                </footer>
-            </aside>
+            <Sidebar />
 
             <main>
                 <form className="create-orphanage-form">
--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
+import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
 import L from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import Sidebar from "../components/Sidebar";
 
 import '../styles/pages/orphanage.css';
 
@@ -18,19 +18,9 @@ const happyMapIcon = L.icon({
 })
 
 export default function Orphanage() {
-    const { goBack } = useHistory();
-
     return (
         <div id="page-orphanage">
-            <aside>
-                <img src={mapMarkerImg} alt="Happy" />
-
-                <footer>
-                    <button type="button" onClick={goBack}>
-                        <FiArrowLeft size={24} color="#FFF" />
-                    </button>
-                </footer>
-            </aside>
+            <Sidebar />
 
             <main>
                 <div className="orphanage-details">
@@ -91,20 +81,20 @@ export default function Orphanage() {
                         <div className="open-details">
                             <div className="hour">
                                 <FiClock size={32} color="#15B6D6" />
-                Segunda à Sexta <br />
-                8h às 18h
-              </div>
+                                Segunda à Sexta <br />
+                                8h às 18h
+                            </div>
                             <div className="open-on-weekends">
                                 <FiInfo size={32} color="#39CC83" />
-                Atendemos <br />
-                fim de semana
-              </div>
+                                Atendemos <br />
+                                fim de semana
+                            </div>
                         </div>
 
                         <button type="button" className="contact-button">
                             <FaWhatsapp size={20} color="#FFF" />
-              Entrar em conctato
-            </button>
+                        Entrar em conctato
+                        </button>
                     </div>
                 </div>
             </main>
