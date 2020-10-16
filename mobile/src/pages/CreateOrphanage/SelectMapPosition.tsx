c0d484821c827dea20f324ceb79ba1bc8dd3ad51
David <davidfilipeamorim@gmail.com>
2020-10-16 23:05:57 +0100

    Add setMapPosition on SelectMapPosition screen

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, MapEvent } from 'react-native-maps';
 
 import mapMarkerImg from '../../images/map-marker.png';
 
 export default function SelectMapPosition() {
     const navigation = useNavigation();
+    const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
     function handleNextStep() {
-        navigation.navigate('OrphanageData');
+        navigation.navigate('OrphanageData', {position});
+    }
+
+    function handleSelectMapPosition(e: MapEvent) {
+        setPosition(e.nativeEvent.coordinate);
     }
 
     return (
@@ -24,16 +29,20 @@ export default function SelectMapPosition() {
                     longitudeDelta: 0.008,
                 }}
                 style={styles.mapStyle}
+                onPress={handleSelectMapPosition}
             >
-                <Marker
-                    icon={mapMarkerImg}
-                    coordinate={{ latitude: 40.6553438, longitude: -7.9118089 }}
-                />
+                {position.latitude !== 0 && (
+                    <Marker
+                        icon={mapMarkerImg}
+                        coordinate={{ latitude: position.latitude, longitude: position.longitude }}
+                    />
+                )}
             </MapView>
-
-            <RectButton style={styles.nextButton} onPress={handleNextStep}>
-                <Text style={styles.nextButtonText}>Próximo</Text>
-            </RectButton>
+            {position.latitude !== 0 && (
+                <RectButton style={styles.nextButton} onPress={handleNextStep}>
+                    <Text style={styles.nextButtonText}>Próximo</Text>
+                </RectButton>
+            )}
         </View>
     )
 }
